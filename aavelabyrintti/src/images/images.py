f34014d5502e672881251b78bf6780be1c7dd65b
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-18 21:25:03 +0200

    images and images.py

---
+++ aavelabyrintti/src/images/images.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Images():
+
+    def __init__(self):
+        self.file_names = ["floor", "wall", "player"]
+
+    def download_images(self):
+        images = []
+        for name in self.file_names:
+            images.append(pygame.image.load("images/" + name + ".png"))
+        return images
