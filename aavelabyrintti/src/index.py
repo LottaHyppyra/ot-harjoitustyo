2a4b18af1c6bd9a5b5cc5255e605a2c4da3be09a
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-18 21:23:42 +0200

    add main()

---
+++ aavelabyrintti/src/index.py
@@ -0,0 +1,37 @@
+from map import Map
+from images.images import Images
+from player import Player
+import pygame
+
+def main():
+    pygame.init()
+    map = Map()
+    images = Images()
+    player = Player(map)
+    pics = images.download_images()
+    scale = pics[0].get_width()
+    screen_height = scale * map.height
+    screen_width = scale * map.width
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Aavelabyrintti")
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player.move(1, 0)
+
+            if event.type == pygame.QUIT:
+                exit()
+
+        screen.fill((0,0,0))
+        for y in range(map.height):
+            for x in range(map.width):
+                box = map.map[y][x]
+                screen.blit(pics[box], (x * scale, y * scale))
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
