be22bc66da2040d6fa657a8f7b462c8e6684e7db
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-28 12:43:03 +0200

    more player tests

--- aavelabyrintti/src/tests/player_test.py
+++ aavelabyrintti/src/tests/player_test.py
@@ -1,15 +1,16 @@
 import unittest
 from player import Player
 
-MAP =  [[1, 1, 1, 1, 1, 1, 1],
-        [1, 2, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1],]
-
 class TestPlayer(unittest.TestCase):
     def setUp(self):
+
+        MAP =  [[1, 1, 1, 1, 1, 1, 1],
+                [1, 2, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1, 1],]
+
         self.test_player = Player(MAP)
 
     def test_get_right_player_coords(self):
@@ -18,3 +19,7 @@ class TestPlayer(unittest.TestCase):
     def test_player_can_move_on_floor(self):
         self.test_player.move(1, 1)
         self.assertEqual(self.test_player.get_coords(), (2, 2))
+
+    def test_player_cant_move_into_wall(self):
+        self.test_player.move(-1, -1)
+        self.assertEqual(self.test_player.get_coords(), (1, 1))
