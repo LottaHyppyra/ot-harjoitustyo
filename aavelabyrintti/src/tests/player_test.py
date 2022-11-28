aefb2d5997c7ec0e4d9b4e0a1a0755587aaa266f
Lotta Hyppyrä <lotta@hyppyra.net>
2022-11-28 12:55:00 +0200

    more player tests

--- aavelabyrintti/src/tests/player_test.py
+++ aavelabyrintti/src/tests/player_test.py
@@ -23,3 +23,15 @@ class TestPlayer(unittest.TestCase):
     def test_player_cant_move_into_wall(self):
         self.test_player.move(-1, -1)
         self.assertEqual(self.test_player.get_coords(), (1, 1))
+
+    def test_returns_none_when_no_player_on_the_map(self):
+
+        MAP2 =  [[1, 1, 1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1, 1],]
+
+        test_player = Player(MAP2)
+        self.assertEqual(test_player.get_coords(), None)
