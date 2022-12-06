d9481a4a7635c4f6fa0309c45d854927bb248a23
Lotta Hyppyrä <lotta@hyppyra.net>
2022-12-06 22:12:26 +0200

    refactoring

--- aavelabyrintti/src/player.py
+++ aavelabyrintti/src/player.py
@@ -15,20 +15,18 @@ class Player():
     def move(self, x_diff, y_diff):
         pos_now = self.get_coords()
 
-        if self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] == FLOOR:
-            self.labyrint[pos_now[1]][pos_now[0]] = FLOOR
-            self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] = PLAYER
-            return False
-
         if self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] == FINISH:
             self.labyrint[pos_now[1]][pos_now[0]] = FLOOR
             return True
+
+        if self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] == FLOOR:
+            self.labyrint[pos_now[1]][pos_now[0]] = FLOOR
+            self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] = PLAYER
 
         if self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] == SMUDGE_STICK:
             self.inventory += 1
             self.labyrint[pos_now[1]][pos_now[0]] = FLOOR
             self.labyrint[pos_now[1] + y_diff][pos_now[0] + x_diff] = PLAYER
-            return False
 
         return False
 
