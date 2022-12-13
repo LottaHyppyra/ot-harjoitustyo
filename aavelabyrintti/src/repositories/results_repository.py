9726c10365ef1769cc52383c51367e50da0a00fe
Lotta Hyppyrä <lotta@hyppyra.net>
2022-12-13 16:24:08 +0200

    add docstring

--- aavelabyrintti/src/repositories/results_repository.py
+++ aavelabyrintti/src/repositories/results_repository.py
@@ -2,12 +2,26 @@ from db import connection
 from init_db import initialize_database
 
 def add_result_to_database(name, moves):
+    """Lisää tuloksen tietokantaan.
+    
+    Args:
+        name: pelaajan nimi.
+        moves: siirtojen määrä.
+
+    """
+
     initialize_database()
     cursor = connection.cursor()
     cursor.execute("INSERT INTO results VALUES (?, ?)", (name, moves))
     connection.commit()
 
 def get_sorted_results():
+    """Järjestää tulokset siirtojen perusteella pienimmästä suurimpaan.
+
+    Returns: Tulokset järjestyksessä.
+
+    """
+    
     cursor = connection.cursor()
     results = cursor.execute("SELECT name, result FROM results").fetchall()
     results.sort(key=lambda result:result[1])
